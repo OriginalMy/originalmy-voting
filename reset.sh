50cd57f91f5cd758a840e306921e1c45a401e657
Edilson Osorio Jr <osoriojr@pm.me>
2020-07-11 22:31:58 +0300

    updated reset.sh

--- reset.sh
+++ reset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e  # Exit immediately if a command exits with a non-zero status.
-#dropdb helios
-#createdb helios
+dropdb -h helios-demo.cpiulccjfpta.us-east-1.rds.amazonaws.com -U postgres helios
+createdb -h helios-demo.cpiulccjfpta.us-east-1.rds.amazonaws.com -U postgres helios
 python manage.py makemigrations
 python manage.py migrate
 echo "from helios_auth.models import User; User.objects.create(user_type='google',user_id='osoriojr@daruni.net', info={'name':'Edilson Osorio Junior'})" | python manage.py shell
