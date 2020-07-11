0908bca372ec517eeded46193a280407850a64e8
Edilson Osorio Jr <osoriojr@pm.me>
2020-07-11 23:04:57 +0300

    VOTER_UPLOAD_REL_PATH = "/tmp/"

--- settings.py
+++ settings.py
@@ -186,7 +186,7 @@ MEDIA_ROOT = ROOT_PATH + "media/"
 
 # a relative path where voter upload files are stored
 # VOTER_UPLOAD_REL_PATH = "voters/%Y/%m/%d"
-VOTER_UPLOAD_REL_PATH = "/tmp/%Y/%m/%d"
+VOTER_UPLOAD_REL_PATH = "/tmp/"
 
 
 # Change your email settings
