00fae5fa38e31a0f735a2d77bed1ba10c0eed84e
Edilson Osorio Jr <osoriojr@pm.me>
2020-07-13 13:49:49 +0300

    SyntaxError: Non-ASCII character '\xc3' in file

--- helios/forms.py
+++ helios/forms.py
@@ -10,33 +10,33 @@ from django.conf import settings
 
 
 class ElectionForm(forms.Form):
-  short_name = forms.SlugField(max_length=40, help_text='sem espaços, fará parte do URL da sua eleição, por exemplo convencao-2020')
-  name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'size':60}), help_text='o nome bonito para sua eleição, por exemplo Eleição do Conselho Administrativo 2010')
+  short_name = forms.SlugField(max_length=40, help_text='sem espacos, fara parte do URL da sua eleicao, por exemplo convencao-2020')
+  name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'size':60}), help_text='o nome bonito para sua eleicao, por exemplo Eleicao do Conselho Administrativo 2010')
   description = forms.CharField(max_length=4000, widget=forms.Textarea(attrs={'cols': 70, 'wrap': 'soft'}), required=False)
   election_type = forms.ChoiceField(label="type", choices = Election.ELECTION_TYPES)
-  use_voter_aliases = forms.BooleanField(required=False, initial=False, help_text='Se selecionado, as identidades dos eleitores serão substituídas por aliases, por exemplo "V12", no centro de rastreamento de votos')
+  use_voter_aliases = forms.BooleanField(required=False, initial=False, help_text='Se selecionado, as identidades dos eleitores serão substituidas por aliases, por exemplo "V12", no centro de rastreamento de votos')
   #use_advanced_audit_features = forms.BooleanField(required=False, initial=True, help_text='disable this only if you want a simple election with reduced security but a simpler user interface')
-  randomize_answer_order = forms.BooleanField(required=False, initial=False, help_text='ative isso se desejar que as respostas às perguntas apareçam em ordem aleatória para cada eleitor')
-  private_p = forms.BooleanField(required=False, initial=False, label="Eleição Privada?", help_text='Uma eleição privada é visível apenas para os eleitores registrados.')
+  randomize_answer_order = forms.BooleanField(required=False, initial=False, help_text='ative isso se desejar que as respostas as perguntas apareçam em ordem aleatoria para cada eleitor')
+  private_p = forms.BooleanField(required=False, initial=False, label="Eleicao Privada?", help_text='Uma eleicao privada e visivel apenas para os eleitores registrados.')
   help_email = forms.CharField(required=False, initial="", label="Email de contato", help_text='Email para que os eleitores possam contatar em caso de necessidade.')
   
   if settings.ALLOW_ELECTION_INFO_URL:
-    election_info_url = forms.CharField(required=False, initial="", label="URL com Detalhes para esta Eleição", help_text="o URL de um documento PDF que contém informações adicionais sobre a eleição, por exemplo biografias e declarações de candidatos")
+    election_info_url = forms.CharField(required=False, initial="", label="URL com Detalhes para esta Eleicao", help_text="o URL de um documento PDF que contem informacoes adicionais sobre a eleicao, por exemplo biografias e declaracoes de candidatos")
   
   # times
-  voting_starts_at = SplitDateTimeField(help_text = 'Data e hora UTC em que a votação começa',
+  voting_starts_at = SplitDateTimeField(help_text = 'Data e hora UTC em que a votacao começa',
                                    widget=SplitSelectDateTimeWidget, required=False)
-  voting_ends_at = SplitDateTimeField(help_text = 'Data e hora UTC em que a votação termina',
+  voting_ends_at = SplitDateTimeField(help_text = 'Data e hora UTC em que a votacao termina',
                                    widget=SplitSelectDateTimeWidget, required=False)
 
 class ElectionTimeExtensionForm(forms.Form):
-  voting_extended_until = SplitDateTimeField(help_text = 'Votação estendida para data e hora do UTC',
+  voting_extended_until = SplitDateTimeField(help_text = 'Votacao estendida para data e hora do UTC',
                                    widget=SplitSelectDateTimeWidget, required=False)
   
 class EmailVotersForm(forms.Form):
   subject = forms.CharField(max_length=80)
   body = forms.CharField(max_length=4000, widget=forms.Textarea)
-  send_to = forms.ChoiceField(label="Enviar para", initial="todos", choices= [('todos', 'todos eleitores'), ('votaram', 'eleitores que já votaram'), ('Não votaram', 'eleitores que ainda não votaram')])
+  send_to = forms.ChoiceField(label="Enviar para", initial="todos", choices= [('todos', 'todos eleitores'), ('votaram', 'eleitores que já votaram'), ('Nao votaram', 'eleitores que ainda nao votaram')])
 
 class TallyNotificationEmailForm(forms.Form):
   subject = forms.CharField(max_length=80)
