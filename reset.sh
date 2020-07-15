d132f4dc9aabae350de88759b078c5950580d2ee
Edilson Osorio Jr <osoriojr@pm.me>
2020-07-15 18:22:12 +0300

    reset.sh

--- reset.sh
+++ reset.sh
@@ -4,4 +4,9 @@ dropdb -h helios-demo.cpiulccjfpta.us-east-1.rds.amazonaws.com -U postgres helio
 createdb -h helios-demo.cpiulccjfpta.us-east-1.rds.amazonaws.com -U postgres helios
 python manage.py makemigrations
 python manage.py migrate
-echo "from helios_auth.models import User; User.objects.create(user_type='google',user_id='osoriojr@daruni.net', info={'name':'Edilson Osorio Junior'})" | python manage.py shell
+echo "from helios_auth.models import User; User.objects.create(user_type='google',user_id='osoriojr@daruni.net',name='Edilson Osorio Junior',admin_p=true, info={'name':'Edilson Osorio Junior','email':'osoriojr@daruni.net'})" | python manage.py shell
+echo "from helios_auth.models import User; User.objects.create(user_type='google',user_id='oshiro.miriam@daruni.net',name='Miriam Tomie Oshiro',admin_p=true, info={'name':'Miriam Tomie Oshiro','email':'oshiro.miriam@daruni.net'})" | python manage.py shell
+echo "from helios_auth.models import User; User.objects.create(user_type='google',user_id='fabiana.marcos@daruni.net',name='Fabiana Marcos',admin_p=true, info={'name':'Fabiana Marcos','email':'fabiana.marcos@daruni.net'})" | python manage.py shell
+echo "from helios_auth.models import User; User.objects.create(user_type='google',user_id='mariana.zanotti@daruni.net',name='Mariana Zanotti',admin_p=true, info={'name':'Mariana Zanotti','email':'mariana.zanotti@daruni.net'})" | python manage.py shell
+echo "from helios_auth.models import User; User.objects.create(user_type='google',user_id='murillo.marques@daruni.net',name='Murillo Marques',admin_p=true, info={'name':'Murillo Marques','email':'murillo.marques@daruni.net'})" | python manage.py shell
+echo "from helios_auth.models import User; User.objects.create(user_type='google',user_id='miranda.uriel@daruni.net',name='Uriel Miranda',admin_p=true, info={'name':'Uriel Miranda','email':'miranda.uriel@daruni.net'})" | python manage.py shell
